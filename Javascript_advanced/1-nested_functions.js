c58f4a8bef19d83cf7bd9b01f6f4f45886aa253b
M6A2R5Y <110352192+M6A2R5Y@users.noreply.github.com>
2023-10-17 12:48:18 +0300

    Calling functions

---
+++ Javascript_advanced/1-nested_functions.js
@@ -0,0 +1,15 @@
+let globalVariable = welcome
+
+function outer() 
+{
+    let course = Holberton
+    function inner() 
+    {
+        let exclamation = !
+        function inception() 
+        {}
+        inception()  //calls the function inception 
+    }
+    inner() //calls the fumction inner
+}
+outer() //calls the function outer
